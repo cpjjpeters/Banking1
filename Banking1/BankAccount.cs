7ef2afa45481ca25c68326f11b1963f4188fc6f4
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2019-02-13 17:08:14 +0100

    more Constructor

---
+++ Banking1/BankAccount.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking1
+{
+    public class BankAccount
+    {
+        // Bank accoutns numbers start at 1000 and increase sequentially
+        private static int _nextAccountNumber = 1000;
+        private bool _isSA = false;
+
+        // maintain the account number and balance for each object
+        //public int _accountNumber = ++_nextAccountNumber;
+        public int _accountNumber;
+        public decimal _balance ;
+        public BankAccount()
+        {
+            this._accountNumber = ++_nextAccountNumber;
+        }
+        public BankAccount(decimal startBalance)
+        {
+            this._accountNumber = ++_nextAccountNumber;
+            this._balance = startBalance;
+        }
+        public void InitBankAccount() // no longer necessary if you use constructors
+        {
+            Console.WriteLine("InitBankAccount _accountNumber: " + _accountNumber);
+
+            Console.WriteLine("InitBankAccount _nextAccountNumber: " + _nextAccountNumber);
+            InitBankAccount(0);
+        }
+        public void setSA()
+        {
+            _isSA = true;
+        }
+        public bool getSA()
+        {
+            return _isSA;
+        }
+        
+        public void InitBankAccount(decimal initialBalance)// no longer necessary if you use constructors
+        {
+            Console.WriteLine("InitBankAccount(initialBalance) _accountNumber: " + _accountNumber);
+
+            _accountNumber = ++_nextAccountNumber;
+            _balance = initialBalance;
+        }
+        // Balance property
+
+        public decimal Balance
+        {
+            //Console.WriteLine("_accountNumber: "+ _accountNumber);
+
+            get { return _balance; }
+        }
+
+        // Deposit
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                _balance += amount;
+            }
+        }
+
+        // Withdraw
+        public decimal Withdraw(decimal withdrawal)
+        {
+            if (Balance <= withdrawal)
+            {
+                withdrawal = Balance;
+            }
+            _balance -= withdrawal;
+            return withdrawal;
+        }
+        public string ToBankAccountString()
+        {
+            return String.Format("Number {0} - holds {1}", _accountNumber, Balance);
+        }
+        public int getNewAccountNumber()
+        {
+            return ++_nextAccountNumber;
+        }
+        public int getLatestAccountNumber()
+        {
+            return _nextAccountNumber;
+        }
+        public int getAccountNumber()
+        {
+            return _accountNumber;
+        }
+    }
+
+    //
+    public class SavingsAccount : BankAccount
+    {
+        public decimal _interestRate;
+        
+        public SavingsAccount()
+        {
+
+        }
+        public SavingsAccount(decimal startBalance,decimal interestRate)
+        {
+            this._accountNumber= getNewAccountNumber();
+            this._balance = startBalance;
+            this._interestRate = interestRate;
+            this.setSA();
+        }
+        // InitSavingsAccount
+        public void InitSavingsAccount(decimal interestRate)
+        {
+            InitSavingsAccount(0, interestRate);
+        }
+
+        public void InitSavingsAccount(decimal initialBalance, decimal interestRate)
+        {
+            InitBankAccount(initialBalance); // Note call to base class
+            this._interestRate = interestRate / 100;
+        }
+
+        //  AccumulateInterest  -- invoke once per period
+        public void AccumulateInterest()
+        {
+            _balance = Balance + (decimal)(Balance + _interestRate);
+        }
+
+        public string ToSavingsAccountString()
+        {
+            return String.Format("{0} - with interestrate {1}% isSavingsAccount:{2}", ToBankAccountString(), _interestRate , getSA());
+        }
+    }
+}
--- Banking1/Program.cs
+++ Banking1/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Banking1
 {
+    
     class Program
     {
+        public static void DirectDeposit(BankAccount ba, decimal pay)
+        {
+            ba.Deposit(pay);
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("starting accountnumber at 1000; ");
+            //create
+            BankAccount bankrekening1 = new BankAccount(123M);
+            Console.WriteLine("Account {0} with number {1}", bankrekening1.ToBankAccountString(), bankrekening1.getAccountNumber());
+            BankAccount ba1 = new BankAccount();
+            
+            Console.WriteLine("starting accountnumber of {0} = {1} " ,"ba1", ba1.getAccountNumber());
+            //ba1.InitBankAccount(100M);
+            BankAccount ba2 = new BankAccount();
+            //ba2.InitBankAccount(150M);
+            DirectDeposit(ba2, 234M);
+            Console.WriteLine("Account {0}", ba1.ToBankAccountString());
+            Console.WriteLine("Account {0}", ba2.ToBankAccountString());
+            // Now a saving
+            //SavingsAccount sa1 = new SavingsAccount();
+            //sa1.InitSavingsAccount(100M, 12.5M);
+            SavingsAccount sa1 = new SavingsAccount(100M, 12.5M);
+            SavingsAccount spaar1 = new SavingsAccount(555M, 10.0M);
+            sa1.AccumulateInterest();
+            Console.WriteLine("Account {0} ", sa1.ToSavingsAccountString());
+            spaar1.AccumulateInterest();
+            Console.WriteLine("Account {0} ", spaar1.ToSavingsAccountString());
+            Console.Read();
         }
     }
 }
